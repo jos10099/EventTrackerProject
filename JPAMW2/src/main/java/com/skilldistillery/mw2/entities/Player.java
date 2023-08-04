04f601a1eb48b6a0da46097a6acbf4a367fa62c9
Jay Perkins <jul3jb@gmail.com>
2023-08-04 16:49:22 -0700

    Create DB, stb MW2 Entity, test hibernate config

---
+++ JPAMW2/src/main/java/com/skilldistillery/mw2/entities/Player.java
@@ -0,0 +1,63 @@
+package com.skilldistillery.mw2.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String userName;
+	
+	public Player() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, userName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		return id == other.id && Objects.equals(userName, other.userName);
+	}
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", userName=" + userName + "]";
+	}
+	
+	
+	
+}
---
+++ JPAMW2/src/test/java/com/skilldistillery/mw2/entities/PlayerTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.mw2.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+	
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Player player;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMW2");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		player = em.find(Player.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		player = null;
+	}
+
+	@Test
+	void test_Player_entity_mapping() {
+		assertNotNull(player);
+		assertEquals("WEEBMILK", player.getUserName());
+	}
+
+}
