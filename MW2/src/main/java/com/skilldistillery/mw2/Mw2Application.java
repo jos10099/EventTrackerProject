ecba344b8fe08971b20c1a59fa2a7b9a04715281
Jay Perkins <jul3jb@gmail.com>
2023-08-04 16:59:35 -0700

    Create JPA for project

---
+++ MW2/src/main/java/com/skilldistillery/mw2/Mw2Application.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mw2Application.class, args);
+	}
+
+}
