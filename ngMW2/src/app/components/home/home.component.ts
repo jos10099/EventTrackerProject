6419ab61cb54e7c9c7cf6fdcd2303910ac279972
Jay Perkins <jul3jb@gmail.com>
2023-08-21 01:30:19 -0700

    added more properties

--- ngMW2/src/app/components/home/home.component.ts
+++ ngMW2/src/app/components/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { PlayerService } from './../../services/player.service';
+import { Player } from './../../models/player';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+playerList: Player[] = [];
+
+  constructor(
+    private playerService: PlayerService
+  ){}
+
+  ngOnInit(): void {
+
+  }
+
+  reload() {
+    this.playerService.index().subscribe({
+      next: (list) => {
+
+      },
+      error: (fail) => {
+        console.error('Home.Component.reload(): error getting players');
+        console.error(fail);
+      }
+    });
+  }
 }
