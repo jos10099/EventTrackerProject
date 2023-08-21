5a8e15cdbf1b9807157a5cf7448f3ad0ccce539f
Jay Perkins <jul3jb@gmail.com>
2023-08-20 22:36:24 -0700

    more changes

--- MW2/src/main/java/com/skilldistillery/mw2/controllers/PlayerController.java
+++ MW2/src/main/java/com/skilldistillery/mw2/controllers/PlayerController.java
@@ -3,6 +3,7 @@ package com.skilldistillery.mw2.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skilldistillery.mw2.entities.Player;
 import com.skilldistillery.mw2.services.PlayerService;
 
+@CrossOrigin({"*", "http://localhost/"})
 @RestController
 @RequestMapping("api")
 public class PlayerController {
